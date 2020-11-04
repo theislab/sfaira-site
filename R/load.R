d9f9d13666f9fd88fe19aa6f4ef2e5189b3bce90
Luke Zappia <lazappi@users.noreply.github.com>
2020-11-04 12:21:37 +0100

    Add datasets table

---
+++ R/load.R
@@ -0,0 +1,31 @@
+get_datasets <- function(datasets_files) {
+
+    `%>%` <- magrittr::`%>%`
+
+    datasets_list <- purrr::map(dataset_files, function (.file) {
+        readr::read_csv(
+            .file,
+            col_types = readr::cols(
+                .default      = readr::col_character(),
+                X1            = readr::col_double(),
+                ncells        = readr::col_double(),
+                year          = readr::col_double(),
+                has_celltypes = readr::col_logical()
+            )
+        )
+    })
+
+    datasets <- datasets_list %>%
+        dplyr::bind_rows() %>%
+        dplyr::mutate(
+            animal    = factor(animal),
+            organ     = factor(organ),
+            subtissue = factor(subtissue),
+            protocol  = factor(protocol)
+        ) %>%
+        dplyr::select(-X1) %>%
+        dplyr::relocate(
+            id, animal, organ, subtissue, ncells, protocol, counts,
+            has_celltypes, lab, year
+        )
+}
